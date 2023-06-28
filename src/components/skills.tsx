3eb1d3dd943890f8556314d26aaff826e4e2d1c2
Craig Dobson <craig@webselect.net>
2023-06-28 21:29:14 +0100

    skills [WIP]

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,18 +2,22 @@
 
 import { Fade } from "react-awesome-reveal";
 import SvgIcon from "./SvgIcon";
+import skillsSvg from "../assets/skills.svg";
 
-const frontendSkills = [
-  "html",
-  "css",
-  "sass",
-  "javascript",
-  "typescript",
-  "react",
-  "vue",
+const portfolioSkills = [
+  {
+    title: "Frontend",
+    skills: ["html", "css", "sass", "javascript", "typescript", "react", "vue"],
+  },
+  {
+    title: "Tooling",
+    skills: ["git", "node", "vite", "webpack", "gulp"],
+  },
+  {
+    title: "Backend",
+    skills: ["node", "dotnet"],
+  },
 ];
-
-const toolingSkills = ["git", "node", "gulp", "webpack", "vite"];
 
 function Hero() {
   return (
@@ -22,45 +26,48 @@ function Hero() {
         <h2 className="mb-5 text-6xl font-black text-white">Skill sets</h2>
         <div className="h-1 m-0 -full bg-gradient-to-l from-rose-950 via-rose-600 to-rose-950 bg-slate-900 animated-gradient"></div>
       </div>
-      <div>
-        <h3 className="mb-10 text-4xl font-black text-white">Frontend</h3>
-        <div className="flex flex-row flex-wrap gap-10 mb-10">
-          <Fade cascade>
-            {frontendSkills.map((item, index) => {
-              return (
-                <SvgIcon
-                  key={index}
-                  iconName={item}
-                  svgProp={{
-                    fill: "currentcolor",
-                    height: "5rem",
-                    width: "5rem",
-                  }}
-                />
-              );
-            })}
-          </Fade>
+
+      <div className="grid items-center justify-center grid-cols-3">
+        <div className="flex flex-col flex-wrap col-span-2 gap-10 mb-10">
+          {portfolioSkills.map((item, index) => {
+            return (
+              <div key={index}>
+                <Fade delay={1000 * index} direction="down" triggerOnce>
+                  <div className="inline-flex flex-col mb-10">
+                    <h3 className="mb-5 text-3xl font-light tracking-widest text-white uppercase">
+                      {item.title}
+                    </h3>
+                    <div className="h-px m-0 -full bg-gradient-to-l from-rose-950 via-rose-600 to-rose-950 bg-slate-900 animated-gradient"></div>
+                  </div>
+                </Fade>
+                <div className="flex flex-row flex-wrap gap-10 mb-10">
+                  <Fade delay={1000 * index} cascade direction="right">
+                    {item.skills.map((skill, index) => {
+                      return (
+                        <SvgIcon
+                          key={index}
+                          iconName={skill}
+                          svgProp={{
+                            fill: "currentcolor",
+                            height: "5rem",
+                            width: "5rem",
+                          }}
+                        />
+                      );
+                    })}
+                  </Fade>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div>
-      <div>
-        <h3 className="mb-10 text-4xl font-black text-white">Tooling</h3>
-        <div className="flex flex-row flex-wrap gap-10 mb-10">
-          <Fade cascade>
-            {toolingSkills.map((item, index) => {
-              return (
-                <SvgIcon
-                  key={index}
-                  iconName={item}
-                  svgProp={{
-                    fill: "currentcolor",
-                    height: "5rem",
-                    width: "5rem",
-                  }}
-                />
-              );
-            })}
-          </Fade>
-        </div>
+        <Fade triggerOnce direction="down" duration={2500}>
+        <img
+          className="w-full max-w-full p-10"
+          src={skillsSvg}
+          alt="Skills Illustration"
+        />
+        </Fade>
       </div>
     </>
   );
